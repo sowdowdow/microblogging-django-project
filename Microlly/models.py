cfdf432f6f032ef4ab61bb3efaf89a814a31e533
sowdowdow <d.simon_97400@hotmail.fr>
2018-11-15 19:33:10 +0100

    added "Post" model

--- Microlly/models.py
+++ Microlly/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=150)
+    body = models.TextField()
+    creation_date = models.DateField(auto_now=False, auto_now_add=True)
+    update_date = models.DateField(auto_now=True, auto_now_add=True)
