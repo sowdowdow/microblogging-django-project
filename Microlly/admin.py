76d7aedd5d4ef33fc43946418258fdc0ddb09a68
sowdowdow <d.simon_97400@hotmail.fr>
2018-11-15 19:55:47 +0100

    Added ModelAdmin for "Post"

--- Microlly/admin.py
+++ Microlly/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
 from Microlly.models import *
 
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("title", "creation_date", "update_date")
+    date_hierarchy = "creation_date"
+    search_fields = ("title",)
+
+
 # Register your models here.
-admin.site.register(Post)
+admin.site.register(Post, PostAdmin)
