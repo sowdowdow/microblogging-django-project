67f17454b6170d405dd30bac0b17f25195203349
sowdowdow <d.simon_97400@hotmail.fr>
2018-11-19 18:25:38 +0100

    Fixed tests... and passed them !

--- Microlly/tests.py
+++ Microlly/tests.py
@@ -8,8 +8,10 @@ class WebsiteTestCase(TestCase):
     fixtures = ["initial.json"]
 
     def test_index_page(self):
+        # getting the wanted page
         response = self.client.get(reverse("Microlly:index"))
-        #self.assertContains(response, "posts")
+        # This one check for text in the rendered html
+        self.assertContains(response, "Liste des dernières publications")
         self.assertEqual(type(response.context["posts"]), QuerySet)
         self.failUnlessEqual(response.status_code, 200)
         self.assertTemplateUsed("Post/index.html")
