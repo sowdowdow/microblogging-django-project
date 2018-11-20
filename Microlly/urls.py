2ffc405f2c02083ccae2c483d735febdc8d9b48f
Sowdowdow <simon.dormeau@etud.u-picardie.fr>
2018-11-20 16:32:56 +0100

    Added Login Template

--- Microlly/urls.py
+++ Microlly/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
+from django.contrib.auth.views import LoginView
 from Microlly import views
-app_name = 'Microlly'
+
+app_name = "Microlly"
 
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path("", views.index, name="index"),
+    path("login/", LoginView.as_view(), name="login"),
 ]
