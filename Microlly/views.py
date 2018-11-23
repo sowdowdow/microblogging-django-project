b4fe9221674107d0525a5830d10f4c4b03bcf2bd
sowdowdow <d.simon_97400@hotmail.fr>
2018-11-23 12:43:35 +0100

    Added empty templates files + Fixed signup view

--- Microlly/views.py
+++ Microlly/views.py
@@ -5,3 +5,6 @@ from django.contrib.auth.models import User
 def index(request):
     posts = Post.objects.all()
     return render(request, 'index.html', {'posts': posts})
+
+def signup(request):
+    return render(request, 'registration/signup.html')
