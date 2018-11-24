59b8f6166607b58f1a5e884f3150a9d08ec6aaae
Sowdowdow <d.simon_97400@hotmail.fr>
2018-11-24 12:32:42 +0100

    Added / fixed  signup view

--- Microlly/forms.py
+++ Microlly/forms.py
@@ -1,7 +1,1 @@
 from django import forms
-
-class SignupForm(forms.Form):
-    username = forms.CharField(label='Pseudo', max_length=25)
-    email = forms.EmailField()
-    password = forms.CharField(label='Mot de passe', widget=forms.PasswordInput)
-    password_confirm = forms.CharField(label='Confirmer le mot de passe', widget=forms.PasswordInput)
--- Microlly/views.py
+++ Microlly/views.py
@@ -1,19 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from Microlly.models import Post
 from Microlly import forms
+from django.contrib.auth import login, authenticate
 from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
 
 def index(request):
     posts = Post.objects.all()
     return render(request, 'index.html', {'posts': posts})
 
 def signup(request):
-    if request.method != 'POST':
-        return render(request, 'registration/signup.html', {'form': forms.SignupForm})
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('home')
     else:
-        posts = request.POST
-        if posts:
-            return render(request, 'registration/signup.html', {'form': forms.SignupForm, 'posts': posts})
-            # TODO :
-            # add tests for datas + implement user saving
-            # https://docs.djangoproject.com/en/2.1/ref/contrib/auth/
+        form = UserCreationForm()
+    return render(request, 'registration/signup.html', {'form': form})
