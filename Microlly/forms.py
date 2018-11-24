985f32a6934b136706584d9a146b9c9f2144c7d2
sowdowdow <d.simon_97400@hotmail.fr>
2018-11-24 10:19:42 +0100

    Added password confirm

--- Microlly/forms.py
+++ Microlly/forms.py
@@ -4,3 +4,4 @@ class SignupForm(forms.Form):
     username = forms.CharField(label='Pseudo', max_length=25)
     email = forms.EmailField()
     password = forms.CharField(label='Mot de passe', widget=forms.PasswordInput)
+    password_confirm = forms.CharField(label='Confirmer le mot de passe', widget=forms.PasswordInput)
