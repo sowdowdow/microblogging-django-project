486003b6b0e43a9019424d4800a859e800174c8d
Sowdowdow <d.simon_97400@hotmail.fr>
2018-12-08 10:27:36 +0100

    Added ckeditor + body is now RTF

--- Microlly/models.py
+++ Microlly/models.py
@@ -1,11 +1,12 @@
 from __future__ import unicode_literals
 from django.db import models
 from django.contrib.auth.models import User
+from ckeditor.fields import RichTextField
 
 # Create your models here.
 class Post(models.Model):
     title = models.CharField(max_length=150)
-    body = models.TextField()
+    body = RichTextField()
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     creation_date = models.DateField(auto_now_add=True)
     update_date = models.DateField(auto_now=True)
--- microblogging/settings.py
+++ microblogging/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     "Microlly",
+    "ckeditor",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
