58b15e86a1a4560e0b814cbd2e48644bf67437cf
Sowdowdow <d.simon_97400@hotmail.fr>
2018-12-09 15:48:53 +0100

    Improved CommentCreateForm + some CSS

--- Microlly/forms.py
+++ Microlly/forms.py
@@ -1,26 +1,31 @@
 from django import forms
 from Microlly import models
 
+
 class PostCreateForm(forms.ModelForm):
     class Meta:
         model = models.Post
-        fields = [
-            'title',
-            'body',
-        ]
+        fields = ["title", "body"]
+
 
 class PostEditForm(forms.ModelForm):
     class Meta:
         model = models.Post
-        fields = [
-            'id',
-            'title',
-            'body',
-        ]
+        fields = ["id", "title", "body"]
+
 
 class CommentCreateForm(forms.ModelForm):
+    message = forms.CharField(
+        required=True, min_length=10, max_length=250, label="", widget=forms.Textarea(
+            attrs={
+                "class": "textarea is-link",
+                "placeholder": "Laisser un commentaire",
+                "rows": 3,
+            }
+        )
+    )
+
     class Meta:
         model = models.Comment
-        fields = [
-            'message',
-        ]
+        fields = ["message"]
+
