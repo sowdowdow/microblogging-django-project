a903194d0aa0e7719ad0b59cd9cd8879db338994
sowdowdow <d.simon_97400@hotmail.fr>
2018-12-10 19:18:19 +0100

    Improved css on login, account and create_post

--- Microlly/forms.py
+++ Microlly/forms.py
@@ -1,8 +1,20 @@
 from django import forms
 from Microlly import models
+from ckeditor.widgets import CKEditorWidget
 
 
 class PostCreateForm(forms.ModelForm):
+    title = forms.CharField(
+        required=True, min_length=5, max_length=150, label="", widget=forms.TextInput(
+            attrs={
+                "class": "input is-link",
+                "placeholder": "Titre de l'article",
+            }
+        )
+    )
+    body = forms.CharField(
+        required=True, min_length=10, label="", widget=CKEditorWidget()
+    )
     class Meta:
         model = models.Post
         fields = ["title", "body"]
@@ -16,7 +28,7 @@ class PostEditForm(forms.ModelForm):
 
 class CommentCreateForm(forms.ModelForm):
     message = forms.CharField(
-        required=True, min_length=10, max_length=250, label="", widget=forms.Textarea(
+        required=True, min_length=5, max_length=250, label="", widget=forms.Textarea(
             attrs={
                 "class": "textarea is-link",
                 "placeholder": "Laisser un commentaire",
